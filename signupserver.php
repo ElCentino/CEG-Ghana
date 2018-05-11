ba32c08cd7b3d15e7e63e0ad53b65b54167e001f
Century <favouronu@gmail.com>
2018-05-11 16:52:21 +0000

    formatted code

--- signupserver.php
+++ signupserver.php
@@ -1,42 +1,47 @@
 <?php 
-require "app/db.php"; 
+
+ require "app/db.php"; 
  session_start();
  $success = array();
  $errors = array();
-   if(isset($_POST['signup_submit'])){
-	   $fullname = $_POST['username'];
-	   $email = $_POST['email'];
-	   $password = $_POST['password'];
-	   $cpassword = $_POST['password2'];
+
+if(isset($_POST['signup_submit'])) {
+
+	$fullname = $_POST['username'];
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	$cpassword = $_POST['password2'];
 	
-	// elseif(){
-	// 	  array_push($errors, "All fiels are required");
-	//   }
-if($fullname !== "" || $email !=="" || $password !=="" ){
-
+	if($fullname !== "" || $email !== "" || $password !== "" ) {
 		
-	if(isset($email)){
-		$select =" SELECT * FROM users WHERE email = '$email'";
-		$rselect = mysqli_query($connect, $select);
-		$rows = mysqli_num_rows($rselect);
-		if($rows == 1){
-			array_push($errors, "E-mail already in use !");
-		 }
-}
-		   $password = md5($password);
-		   $query = " INSERT INTO users (fullname, email, password) VALUES ('$fullname', '$email', '$password')";
-		   if($connect->query($query)==TRUE){
-			   array_push($success, "Succesfuly registered !");
+		if(isset($email)) {
 
-		   }else{
-			   array_push($errors, "Sorry try again later");
-		   }
+			$select =" SELECT * FROM users WHERE email = '$email'";
+			$rselect = mysqli_query($connect, $select);
+			$rows = mysqli_num_rows($rselect);
 
-	   }else{
+			if($rows == 1){
+				array_push($errors, "E-mail already in use !");
+			}
+		}
+
+		$password = md5($password);
+		$query = " INSERT INTO users (fullname, email, password) VALUES ('$fullname', '$email', '$password')";
+
+		if($connect->query($query) == TRUE) {
+
+			array_push($success, "Succesfuly registered !");
+
+		} else {
+
+			array_push($errors, "Sorry try again later");
+		}
+
+	} else {
+
 		array_push($errors, "All fields are required");
-	   }
-	   $connect->close();
 	}
-  
 
+	$connect->close();
+}
 ?>
